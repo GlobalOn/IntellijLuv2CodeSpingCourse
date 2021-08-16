4b17d7bdd6339b202f7f8673d064f025af672cbb
GlobalOn <urkin961@gmail.com>
2021-08-16 19:07:51 +0300

    Rough Prototype.

---
+++ src/com/luv2code/springdemo/MyApp.java
@@ -0,0 +1,12 @@
+package com.luv2code.springdemo;
+
+public class MyApp {
+    public static void main(String[] args) {
+
+        Coach baseballCoach = new BaseballCoach();
+        Coach trackCoach = new TrackCoach();
+
+        System.out.println(baseballCoach.getDailyWorkout());
+        System.out.println(trackCoach.getDailyWorkout());
+    }
+}
